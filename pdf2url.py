3691eefc78dd2216993854344ded63652139f8bf
Ag3nt047 <58594630+ATTACKnDEFEND@users.noreply.github.com>
2023-06-14 16:29:45 +1000

    standardise arguments

--- pdf2url.py
+++ pdf2url.py
@@ -3,7 +3,6 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.colors import black
 from reportlab.pdfbase.pdfmetrics import stringWidth
 
-
 def create_pdf(file_name, text, url):
     # Create a PDF canvas
     c = canvas.Canvas(file_name)
@@ -36,16 +35,14 @@ def create_pdf(file_name, text, url):
 
     print("PDF created successfully.")
 
-
 def main():
     parser = argparse.ArgumentParser(description="Create a PDF with a clickable link.")
     parser.add_argument("-t", "--text", default="Click me", help="Text shown for the link")
     parser.add_argument("-u", "--url", required=True, help="URL to be opened when the link is clicked")
-    parser.add_argument("-f", "--file_name", default="output.pdf", help="Name of the PDF file to be saved (default: output.pdf)")
+    parser.add_argument("-o", "--output", default="output.pdf", help="Name of the PDF file to be saved (default: output.pdf)")
     args = parser.parse_args()
 
-    create_pdf(args.file_name, args.text, args.url)
-
+    create_pdf(args.output, args.text, args.url)
 
 if __name__ == "__main__":
     main()
